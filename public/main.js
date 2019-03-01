8613644e2457f08a92433b8a88a2f583146129b4
Johannes Lindén <johannes@godesity.se>
2019-03-01 20:23:26 +0100

    Change socketio url to origin

--- public/main.js
+++ public/main.js
@@ -87,7 +87,7 @@ var timeline = new vis.Timeline(container, items, options);
 
 // Socket stuff down below
 var sessionName = location.pathname.split('/').slice(-1)[0];
-var socket = io(`localhost:${location.port}/${sessionName}`);
+var socket = io(`${location.origin}:${location.port}/${sessionName}`);
 
 socket.on('add', function (item) {
 	var oldItem = items.get(item.id);
